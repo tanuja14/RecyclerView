dbd3c25b750365558f674906da1099d9d48d465f
Tanuja Bhatnagar <tanuja.20489@ggnindia.dronacharya.info>
2021-10-24 13:42:37 +0530

    first commit

---
+++ android-basics-kotlin-dogglers-app-main/app/src/main/java/com/example/dogglers/adapter/DogCardAdapter.kt
@@ -0,0 +1,100 @@
+/*
+* Copyright (C) 2021 The Android Open Source Project.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+package com.example.dogglers.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.dogglers.R
+import com.example.dogglers.data.DataSource.dogs
+import com.example.dogglers.model.Dog
+
+
+/**
+ * Adapter to inflate the appropriate list item layout and populate the view with information
+ * from the appropriate data source
+ */
+class DogCardAdapter(
+    private val context: Context?,
+    private val layout: Int
+): RecyclerView.Adapter<DogCardAdapter.DogCardViewHolder>() {
+
+    // TODO: Initialize the data using the List found in data/DataSource
+    val MyDataset= dogs
+
+    /**
+     * Initialize view elements
+     */
+    class DogCardViewHolder(view: View?): RecyclerView.ViewHolder(view!!) {
+
+        val dogImage: ImageView= view!!.findViewById(R.id.image)
+        val dogName:TextView= view!!.findViewById(R.id.name)
+        val dogAge:TextView= view!!.findViewById(R.id.age)
+        val dogHobby:TextView= view!!.findViewById(R.id.hobbies)
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DogCardViewHolder {
+        // TODO: Use a conditional to determine the layout type and set it accordingly.
+        //  if the layout variable is Layout.GRID the grid list item should be used. Otherwise the
+        //  the vertical/horizontal list item should be used.
+        val adapterLayout=when(viewType){
+
+            2,1->{
+                 LayoutInflater.from(parent.context)
+                    .inflate(R.layout.vertical_horizontal_list_item, parent, false)
+            }
+            else -> {
+                LayoutInflater.from(parent.context)
+                    .inflate(R.layout.grid_list_item, parent, false)
+            }
+
+        }
+
+        // TODO Inflate the layout
+
+        // TODO: Null should not be passed into the view holder. This should be updated to reflect
+        //  the inflated layout.
+        return DogCardViewHolder(adapterLayout)
+    }
+
+    override fun getItemCount(): Int = MyDataset.size // TODO: return the size of the data set instead of 0
+
+    override fun onBindViewHolder(holder: DogCardViewHolder, position: Int) {
+        // TODO: Get the data at the current position
+        // TODO: Set the image resource for the current dog
+        // TODO: Set the text for the current dog's name
+        // TODO: Set the text for the current dog's age
+
+        val item=MyDataset[position]
+
+        val resources = context?.resources
+        holder.dogImage.setImageResource(item.imageResourceId)
+        holder.dogName.text = item.name
+        holder.dogAge.text = resources?.getString(R.string.dog_age, item.age)
+        holder.dogHobby.text = resources?.getString(R.string.dog_hobbies, item.hobbies)
+        // TODO: Set the text for the current dog's hobbies by passing the hobbies to the
+        //  R.string.dog_hobbies string constant.
+        //  Passing an argument to the string resource looks like:
+        //  resources?.getString(R.string.dog_hobbies, dog.hobbies)
+    }
+}
+
+
